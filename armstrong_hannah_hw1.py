daafd286e76e471bb14f20c233408005d5be0ecb
Hannah Armstrong <hannah.armstrong1901@gmail.com>
2021-01-30 23:21:40 -0800

    Added error handled input.

--- armstrong_hannah_hw1.py
+++ armstrong_hannah_hw1.py
@@ -1,4 +1,11 @@
+isGood = False
 num = input("Enter a number:")
+while not isGood:
+    if num.isdigit():
+        if int(num) >= 0:
+            isGood = True
+    if not isGood:
+        num = input("Incorrect input. Enter a number:")
 num = int(num)
 if num % 4 == 0:
     if num % 100 == 0:
